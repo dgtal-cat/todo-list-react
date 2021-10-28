72c8ac5f3e44e8faa6ec09642da7abf5a3a592a7
Aleksey Kremnev <dgtal.cat@gmail.com>
2021-10-28 21:27:34 +0300

    adding classes

--- src/components/task.js
+++ src/components/task.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 
 import "./task.css"
 
-const Task = ({description, created, important = false, completed = false, editing = false}) => {
+export default class Task extends Component {
 
-   return (
-      <div className="view">
-         <input className="toggle" type="checkbox" />
+   state = {
+      description: "",
+      created: "",
+      important: false,
+      completed: false,
+      editing: false
+   }
+
+   onClick = () => {
+
+   }
+
+   render() {
+      const {description, created, important = false, completed = false, editing = false} = this.props
+
+      return (
+         <div className="view">
+            <input className="toggle" type="checkbox" onClick={this.onClick} />
             <label>
                <span className="description">{description}</span>
                <span className="created">{created}</span>
             </label>
             <button className="icon icon-edit"/>
             <button className="icon icon-destroy"/>
-      </div>
-   )
+         </div>
+      )
+   }
 }
-
-export default Task
